d43b9e6400336d278a3f421a1af38835f5d8d258
qwewqa <17557038+qwewqa@users.noreply.github.com>
2019-09-09 16:53:16 -0400

    Allow stage without running

--- src/main/kotlin/tools/qwewqa/sim/extensions/Stage.kt
+++ src/main/kotlin/tools/qwewqa/sim/extensions/Stage.kt
@@ -4,13 +4,12 @@ import kotlinx.coroutines.runBlocking
 import tools.qwewqa.sim.stage.Stage
 
 /**
- * Initializes a stage with [init] and runs it blocking
+ * Initializes a stage with [init]
  */
-fun stage(init: Stage.() -> Unit) {
-    val stage = Stage()
-    stage.init()
-    runBlocking { stage.run() }
-}
+fun stage(startNow: Boolean = true, init: Stage.() -> Unit) =
+    Stage().apply(init).also { if (startNow) runBlocking { it.run() } }
 
 fun Stage.endIn(time: Double) = timeline.schedule(time) { end() }
-fun Stage.onEnd(action: Stage.() -> Unit) { this.onEnd = action }
+fun Stage.onEnd(action: Stage.() -> Unit) {
+    this.onEnd = action
+}
