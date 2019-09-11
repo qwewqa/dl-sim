5658ecd969401801f02ac6a9271e963271dfb0ad
qwewqa <17557038+qwewqa@users.noreply.github.com>
2019-09-11 18:33:48 -0400

    Fix lowercase in data map

--- src/main/kotlin/tools/qwewqa/sim/data/LooseMap.kt
+++ src/main/kotlin/tools/qwewqa/sim/data/LooseMap.kt
@@ -3,6 +3,10 @@ package tools.qwewqa.sim.data
 open class LooseMap<T>(private val map: MutableMap<String, T> = mutableMapOf()) : MutableMap<String, T> by map {
     override operator fun get(key: String) = map[key.toLowerCase()] ?: error("Unknown key $key")
     open operator fun set(vararg names: String, value: T) {
-        names.forEach { map[it.toLowerCase()] = value }
+        names.forEach { this[it.toLowerCase()] = value }
+    }
+    operator fun set(key: String, value: T) {
+        if (map.containsKey(key.toLowerCase())) error("$key already exists")
+        else map[key.toLowerCase()] = value
     }
 }
