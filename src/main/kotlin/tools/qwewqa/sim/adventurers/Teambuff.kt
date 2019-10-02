f2b2a384482e3674ea48bb2198d55aac4b3e0942
qwewqa <17557038+qwewqa@users.noreply.github.com>
2019-10-02 02:04:50 +0000

    Don't disp teambuff if none

--- src/main/kotlin/tools/qwewqa/sim/adventurers/Teambuff.kt
+++ src/main/kotlin/tools/qwewqa/sim/adventurers/Teambuff.kt
@@ -66,6 +66,6 @@ val teambuff = AdventurerSetup {
 
     stage.onEnd {
         total += str * lastCount * (time - lastTime)
-        enemy.damage(Snapshot(total, 0, 0.0, listOf("Teambuff")), false)
+        if (total > 0) enemy.damage(Snapshot(total, 0, 0.0, listOf("Teambuff")), false)
     }
 }
