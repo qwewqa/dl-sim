c4244f4150f3aa9057d58dd23a59d21218c99d04
qwewqa <17557038+qwewqa@users.noreply.github.com>
2019-10-04 22:08:23 +0000

    New operators and precedence

--- src/main/kotlin/tools/qwewqa/sim/stage/Acl.kt
+++ src/main/kotlin/tools/qwewqa/sim/stage/Acl.kt
@@ -4,6 +4,7 @@ import tools.qwewqa.sim.data.Debuffs
 import tools.qwewqa.sim.wep.blade
 import tools.qwewqa.sim.wep.lance
 import tools.qwewqa.sim.wep.wand
+import java.util.*
 
 class AclSelector(val adventurer: Adventurer) {
     class SkillData(val charge: Int, val remaining: Int, val ready: Boolean)
@@ -81,11 +82,8 @@ data class AclLine(val move: String, val condition: List<String>)
 
 fun parseLine(string: String): AclLine {
     val skill = string.substringBefore(",").trim()
-    val conditionText = string.substringAfter(",", "").trim()
-    val condition =
-        if (conditionText.isEmpty() && skill in listOf("s1", "s2", "s3")) listOf("default") else parseCondition(
-            conditionText
-        )
+    val conditionText = string.substringAfter(",", "default").trim()
+    val condition = parseCondition(conditionText)
     return AclLine(skill, condition)
 }
 
@@ -102,23 +100,20 @@ fun AclSelector.parseSkill(name: String) = when (name) {
 
 fun parseCondition(string: String): List<String> {
     if (string.isEmpty()) return emptyList()
+    val specialTokens = listOf("!", "&&", "||", "!=", ">=", "<=", ">", "<", "=", "==", "(", ")", "+", "-", "*", "/")
     val tokens = tokenizeCondition(string)
     val output = mutableListOf<String>()
     val stack = mutableListOf<String>()
     tokens.forEach {
-        when (it) {
-            "(" -> stack += "("
-            "and" -> stack += "&&"
-            "or" -> stack += "||"
-            "&&" -> stack += "&&"
-            "||" -> stack += "||"
-            ")" -> {
+        when {
+            it == ")" -> {
                 while (stack.last() != "(") {
                     output += stack.last()
                     stack.removeAt(stack.lastIndex)
                 }
                 stack.removeAt(stack.lastIndex)
             }
+            it in specialTokens -> stack += it
             else -> output += it
         }
     }
@@ -126,35 +121,128 @@ fun parseCondition(string: String): List<String> {
     return output
 }
 
-fun AclSelector.evaluateConditions(conditions: List<String>): Boolean {
-    if (conditions.isEmpty()) return true
-    val results = mutableListOf<Boolean>()
-    conditions.forEach {
-        when (it) {
-            "&&" -> {
-                val i = results.lastIndex
-                val result = results[i] && results[i - 1]
-                results.removeAt(i)
-                results.removeAt(i - 1)
-                results.add(result)
+fun tokenizeCondition(string: String): List<String> {
+    var current = ""
+    var remaining = string + " "
+    val tokens = mutableListOf<String>()
+    val specialChars = listOf('!', '+', '-', '*', '/', '=', '&', '|', '(', ')')
+    val specialTokens = listOf("!", "&&", "||", "!=", ">=", "<=", ">", "<", "=", "==", "(", ")", "+", "-", "*", "/")
+    val replacements = mapOf("=" to "==", "and" to "&&", "or" to "||")
+    while (remaining.isNotEmpty()) {
+        val nextChar = remaining[0]
+        val next = current + nextChar
+        remaining = remaining.drop(1)
+        when {
+            next !in specialTokens && current in specialTokens -> {
+                tokens += current
+                current = if (nextChar.isWhitespace()) "" else nextChar.toString()
             }
-            "||" -> {
-                val i = results.lastIndex
-                val result = results[i] || results[i - 1]
-                results.removeAt(i)
-                results.removeAt(i - 1)
-                results.add(result)
+            nextChar in specialChars && next !in specialTokens && current !in specialTokens && current.isNotEmpty() -> {
+                tokens += current
+                current = nextChar.toString()
+            }
+            nextChar.isWhitespace() -> {
+                if (current.isNotEmpty()) {
+                    tokens += current
+                    current = ""
+                }
             }
             else -> {
-                results.add(evaluateSingleCondition(it))
+                current = next
             }
         }
     }
-    check(results.size == 1)
-    return results[0]
+    fun String.infix(precedence: Int) = mutableListOf<String>().apply {
+        repeat(precedence) { add(")") }
+        add(this@infix)
+        repeat(precedence) { add("(") }
+    }
+    return (listOf("(") + tokens.map { replacements[it] ?: it } + listOf(")")).map {
+        when (it) {
+            "*" -> it.infix(1)
+            "/" -> it.infix(1)
+            "+" -> it.infix(2)
+            "-" -> it.infix(2)
+            "==" -> it.infix(3)
+            "!=" -> it.infix(3)
+            ">=" -> it.infix(3)
+            "<=" -> it.infix(3)
+            ">" -> it.infix(3)
+            "<" -> it.infix(3)
+            "&&" -> it.infix(4)
+            "||" -> it.infix(4)
+            "(" -> listOf("(", "(", "(", "(", "(")
+            ")" -> listOf(")", ")", ")", ")", ")")
+            else -> listOf(it)
+        }
+    }.reduce { a, b -> a + b }
 }
 
-fun AclSelector.evaluateSingleCondition(name: String): Boolean = when (name.toLowerCase()) {
+@Suppress("UNCHECKED_CAST")
+fun AclSelector.evaluateConditions(conditions: List<String>): Boolean {
+    if (conditions.isEmpty()) return true
+    val results = ArrayDeque<Any>()
+    conditions.forEach {
+        results.push(
+            when (it) {
+                "&&" -> {
+                    val b = results.pop().let { (it as? Boolean) ?: (((it as? Int)?.toDouble() ?: (it as Double)) > 0) }
+                    val a = results.pop().let { (it as? Boolean) ?: (((it as? Int)?.toDouble() ?: (it as Double)) > 0) }
+                    a && b
+                }
+                "||" -> {
+                    val b = results.pop().let { (it as? Boolean) ?: (((it as? Int)?.toDouble() ?: (it as Double)) > 0) }
+                    val a = results.pop().let { (it as? Boolean) ?: (((it as? Int)?.toDouble() ?: (it as Double)) > 0) }
+                    a || b
+                }
+                "!" -> {
+                    !results.pop().let { (it as? Boolean) ?: (((it as? Int)?.toDouble() ?: (it as Double)) > 0) }
+                }
+                else -> {
+                    if (it in listOf(">=", "<=", ">", "<", "==", "!=", "+", "-", "*", "/")) {
+                        val b = results.pop()
+                        val a = results.pop()
+                        if (a is Int && b is Int) {
+                            when (it) {
+                                ">=" -> a >= b
+                                "<=" -> a <= b
+                                ">" -> a > b
+                                "<" -> a < b
+                                "==" -> a == b
+                                "!=" -> a != b
+                                "+" -> a + b
+                                "-" -> a - b
+                                "*" -> a * b
+                                "/" -> a / b
+                                else -> error("error")
+                            }
+                        } else {
+                            val bd = (a as? Int)?.toDouble() ?: a as Double
+                            val ad = (b as? Int)?.toDouble() ?: b as Double
+                            when (it) {
+                                ">=" -> ad >= bd
+                                "<=" -> ad <= bd
+                                ">" -> ad > bd
+                                "<" -> ad < bd
+                                "==" -> ad == bd
+                                "!=" -> ad != bd
+                                "+" -> ad + bd
+                                "-" -> ad - bd
+                                "*" -> ad * bd
+                                "/" -> ad / bd
+                                else -> error("error")
+                            }
+                        }
+                    } else evaluateSingleCondition(it)
+                }
+            }
+        )
+    }
+    check(results.size == 1)
+    return results.pop() as Boolean
+}
+
+fun AclSelector.evaluateSingleCondition(name: String): Any = when (name.toLowerCase()) {
     "default" -> default
     "cancel" -> cancel
     "s1.ready" -> s1info.ready
@@ -162,43 +250,6 @@ fun AclSelector.evaluateSingleCondition(name: String): Boolean = when (name.toLo
     "s3.ready" -> s3info.ready
     "s1transform" -> adventurer.s1Transform
     "s2transform" -> adventurer.s2Transform
-    else -> when {
-        name[0] == '!' -> !evaluateSingleCondition(name.drop(1))
-        name.contains(">=") -> {
-            val first = evaluateValue(name.substringBefore(">="))
-            val second = evaluateValue(name.substringAfter(">="))
-            first >= second
-        }
-        name.contains("<=") -> {
-            val first = evaluateValue(name.substringBefore("<="))
-            val second = evaluateValue(name.substringAfter("<="))
-            first <= second
-        }
-        name.contains(">") -> {
-            val first = evaluateValue(name.substringBefore(">"))
-            val second = evaluateValue(name.substringAfter(">"))
-            first > second
-        }
-        name.contains("<") -> {
-            val first = evaluateValue(name.substringBefore("<"))
-            val second = evaluateValue(name.substringAfter("<"))
-            first < second
-        }
-        name.contains("!=") -> {
-            val first = evaluateValue(name.substringBefore("!="))
-            val second = evaluateValue(name.substringAfter("!="))
-            first != second
-        }
-        name.contains("=") -> {
-            val first = evaluateValue(name.substringBefore("="))
-            val second = evaluateValue(name.substringAfter("="))
-            first == second
-        }
-        else -> +name
-    }
-}
-
-fun AclSelector.evaluateValue(name: String) = when (name.toLowerCase()) {
     "seq" -> seq
     "hp" -> (adventurer.hp * 100).toInt()
     "combo" -> adventurer.combo
@@ -216,30 +267,7 @@ fun AclSelector.evaluateValue(name: String) = when (name.toLowerCase()) {
     "od_remaining" -> adventurer.enemy.odRemaining
     else -> when {
         name.toIntOrNull() != null -> name.toIntOrNull()!!
-        else -> error("unknown value $name")
+        name.toDoubleOrNull() != null -> name.toDoubleOrNull()!!
+        else -> +name
     }
 }
-
-fun tokenizeCondition(string: String) = string
-    .replace("(", " ( ")
-    .replace(")", " ) ")
-    .replace("\\s+".toRegex(), " ")
-    .replace("==", "=")
-    .replace(" =", "=")
-    .replace(" >", ">")
-    .replace(" <", "<")
-    .replace(" >=", ">=")
-    .replace(" <=", "<=")
-    .replace(" !=", "!=")
-    .replace("= ", "=")
-    .replace("> ", ">")
-    .replace("< ", "<")
-    .replace(">= ", ">=")
-    .replace("<= ", "<=")
-    .replace("!= ", "!=")
-    .replace("&&", " && ")
-    .replace("||", " || ")
-    .replace("\\s+".toRegex(), " ")
-    .replace("! ", "!")
-    .trim()
-    .split(" ")
